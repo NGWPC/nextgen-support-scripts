8a5571346b057d28342bcb68ad49f33b8adeda03
Miguel Pena <miguelp1986@gmail.com>
2025-10-08 11:27:33 -0700

    downloading extract_ngen_cli script in user bootstrap script

--- parallel_works_scripts/user_bootstrap.sh
+++ parallel_works_scripts/user_bootstrap.sh
@@ -8,15 +8,23 @@ PY_PIP_PKGS="Flask gunicorn"
 
 # set startup_cluster script paths
 REPO_URL="https://raw.githubusercontent.com/NGWPC/nwm-automation-scripts/development/parallel_works_scripts"
-SCRIPT_PATH="startup_cluster.sh"
-LOCAL_SCRIPT="/tmp/${SCRIPT_PATH}"
+STARTUP_SCRIPT="startup_cluster.sh"
+STARTUP_SCRIPT_PATH="/tmp/${STARTUP_SCRIPT}"
+
+# also fetch extract_ngen_cli script from the same directory
+EXTRACT_SCRIPT="extract_ngen_cli.sh"
+EXTRACT_SCRIPT_PATH="/tmp/${EXTRACT_SCRIPT}"
 
 # fetch startup_cluster script
-curl -fsSL --retry 3 --retry-connrefused "${REPO_URL}/${SCRIPT_PATH}" -o "$LOCAL_SCRIPT"
-chmod +x "$LOCAL_SCRIPT"
+curl -fsSL --retry 3 --retry-connrefused "${REPO_URL}/${STARTUP_SCRIPT}" -o "$STARTUP_SCRIPT_PATH"
+chmod +x "$STARTUP_SCRIPT_PATH"
+
+# fetch extract_ngen_cli script
+curl -fsSL --retry 3 --retry-connrefused "${REPO_URL}/${EXTRACT_SCRIPT}" -o "$EXTRACT_SCRIPT_PATH"
+chmod +x "$EXTRACT_SCRIPT_PATH"
 
 # run script with environment variables
 ADD_USERS="$ADD_USERS" \
 DNF_PACKAGES_EXTRA="$DNF_PACKAGES_EXTRA" \
 PY_PIP_PKGS="$PY_PIP_PKGS" \
-"$LOCAL_SCRIPT"
+"$STARTUP_SCRIPT_PATH"
