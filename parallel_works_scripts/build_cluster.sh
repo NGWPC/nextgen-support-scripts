157b14602689d731612cc9f0f2e98ce3a2a45ebc
Chris Nealen <christopher.nealen@rtx.com>
2025-12-18 21:41:12 -0500

    added  caching for development and feature builds and for faster testing

--- parallel_works_scripts/build_cluster.sh
+++ parallel_works_scripts/build_cluster.sh
@@ -1052,7 +1052,7 @@ if [[ "$BUILD_TYPE" == "development" ]]; then
                     if [[ -d "${BASE_PATH}/nwm-cal-mgr" ]]; then
                         update_repo_branch "nwm-cal-mgr" "development"
                         echo "[$(date '+%H:%M:%S')] Building nwm-cal-mgr (development) Docker image with NGEN_IMAGE_TAG=latest"
-                        docker build --progress=plain --no-cache \
+                        docker build --progress=plain \
                             --build-arg NGEN_IMAGE_TAG="latest" \
                             --tag="${REGISTRY}/nwm-cal-mgr:latest" \
                             "${BASE_PATH}/nwm-cal-mgr"
@@ -1067,7 +1067,7 @@ if [[ "$BUILD_TYPE" == "development" ]]; then
                     if [[ -d "${BASE_PATH}/nwm-fcst-mgr" ]]; then
                         update_repo_branch "nwm-fcst-mgr" "development"
                         echo "[$(date '+%H:%M:%S')] Building nwm-fcst-mgr (development) Docker image with NGEN_IMAGE_TAG=latest"
-                        docker build --progress=plain --no-cache \
+                        docker build --progress=plain \
                             --build-arg NGEN_IMAGE_TAG="latest" \
                             --tag="${REGISTRY}/nwm-fcst-mgr:latest" \
                             "${BASE_PATH}/nwm-fcst-mgr"
@@ -1082,7 +1082,7 @@ if [[ "$BUILD_TYPE" == "development" ]]; then
                     if [[ -d "${BASE_PATH}/nwm-verf" ]]; then
                         update_repo_branch "nwm-verf" "development"
                         echo "[$(date '+%H:%M:%S')] Building nwm-verf (development) Docker image"
-                        docker build --progress=plain --no-cache \
+                        docker build --progress=plain \
                             --build-arg NWM_EVAL_MGR_TAG="development" \
                             --tag="${REGISTRY}/nwm-verf:latest" \
                             "${BASE_PATH}/nwm-verf"
@@ -1096,7 +1096,7 @@ if [[ "$BUILD_TYPE" == "development" ]]; then
                     if [[ "${IMAGE_SOURCE[ngen-forcing]}" == "build" ]]; then
                         update_repo_branch "ngen-forcing" "development"
                         echo "[$(date '+%H:%M:%S')] Building ngen-bmi-forcing (development) Docker image"
-                        docker build --progress=plain --no-cache \
+                        docker build --progress=plain \
                             --file "${BASE_PATH}/ngen-forcing/Dockerfile.bmi-forcings" \
                             --tag="${REGISTRY}/ngen-bmi-forcing:latest" \
                             "${BASE_PATH}/ngen-forcing"
@@ -1114,7 +1114,7 @@ if [[ "$BUILD_TYPE" == "development" ]]; then
                     if [[ "${IMAGE_SOURCE[ngen]}" == "build" ]]; then
                         update_repo_branch "ngen" "development"
                         echo "[$(date '+%H:%M:%S')] Building ngen (development) Docker image with NGEN_FORCING_TAG=latest"
-                        docker build --progress=plain --no-cache \
+                        docker build --progress=plain \
                             --build-arg NGEN_FORCING_TAG="latest" \
                             --tag="${REGISTRY}/ngen:latest" \
                             "${BASE_PATH}/ngen"
@@ -1172,7 +1172,7 @@ if [[ "$BUILD_TYPE" == "feature" ]]; then
             forcing_docker_tag="$(get_docker_tag_for_repo "ngen-forcing" "$BUILD_TYPE")"
 
             echo "[$(date '+%H:%M:%S')] Building ngen-bmi-forcing (${forcing_docker_tag}) Docker image"
-            docker build --progress=plain --no-cache \
+            docker build --progress=plain \
                 --file "${BASE_PATH}/ngen-forcing/Dockerfile.bmi-forcings" \
                 --tag="${REGISTRY}/ngen-bmi-forcing:${forcing_docker_tag}" \
                 "${BASE_PATH}/ngen-forcing"
@@ -1193,7 +1193,7 @@ if [[ "$BUILD_TYPE" == "feature" ]]; then
             ngen_forcing_docker_tag="$(get_docker_tag_for_repo "ngen-forcing" "$BUILD_TYPE")"
 
             echo "[$(date '+%H:%M:%S')] Building ngen (${ngen_docker_tag}) Docker image with NGEN_FORCING_TAG=${ngen_forcing_docker_tag}"
-            docker build --progress=plain --no-cache \
+            docker build --progress=plain \
                 --build-arg NGEN_FORCING_TAG="${ngen_forcing_docker_tag}" \
                 --tag="${REGISTRY}/ngen:${ngen_docker_tag}" \
                 "${BASE_PATH}/ngen"
@@ -1223,7 +1223,7 @@ if [[ "$BUILD_TYPE" == "feature" ]]; then
                     ngen_docker_tag="$(get_docker_tag_for_repo "ngen" "$BUILD_TYPE")"
 
                     echo "[$(date '+%H:%M:%S')] Building nwm-cal-mgr (${cal_mgr_docker_tag}) Docker image with NGEN_IMAGE_TAG=${ngen_docker_tag}"
-                    docker build --progress=plain --no-cache \
+                    docker build --progress=plain \
                         --build-arg NGEN_IMAGE_TAG="${ngen_docker_tag}" \
                         --tag="${REGISTRY}/nwm-cal-mgr:${cal_mgr_docker_tag}" \
                         "${BASE_PATH}/nwm-cal-mgr"
@@ -1242,7 +1242,7 @@ if [[ "$BUILD_TYPE" == "feature" ]]; then
                     ngen_docker_tag="$(get_docker_tag_for_repo "ngen" "$BUILD_TYPE")"
 
                     echo "[$(date '+%H:%M:%S')] Building nwm-fcst-mgr (${fcst_mgr_docker_tag}) Docker image with NGEN_IMAGE_TAG=${ngen_docker_tag}"
-                    docker build --progress=plain --no-cache \
+                    docker build --progress=plain \
                         --build-arg NGEN_IMAGE_TAG="${ngen_docker_tag}" \
                         --tag="${REGISTRY}/nwm-fcst-mgr:${fcst_mgr_docker_tag}" \
                         "${BASE_PATH}/nwm-fcst-mgr"
@@ -1264,7 +1264,7 @@ if [[ "$BUILD_TYPE" == "feature" ]]; then
                     fi
 
                     echo "[$(date '+%H:%M:%S')] Building nwm-verf (${verf_docker_tag}) Docker image with NWM_EVAL_MGR_TAG=${nwm_eval_mgr_branch}"
-                    docker build --progress=plain --no-cache \
+                    docker build --progress=plain \
                         --build-arg NWM_EVAL_MGR_TAG="${nwm_eval_mgr_branch}" \
                         --tag="${REGISTRY}/nwm-verf:${verf_docker_tag}" \
                         "${BASE_PATH}/nwm-verf"
