7abb6050bb41956bc4b3f56d8af97653b3d9aa88
Miguel Pena <miguelp1986@gmail.com>
2026-02-18 11:20:36 -0800

    update fix pw password file

--- fix_pw_password.sh
+++ fix_pw_password.sh
@@ -7,7 +7,9 @@ sudo chown "${USER}:pwuser" -R /ngencerf-app/nwm-fcst-mgr
 sudo chown "${USER}:pwuser" -R /ngencerf-app/nwm-verf
 sudo chown "${USER}:pwuser" -R /ngencerf-app/ngencerf-server
 sudo chown "${USER}:pwuser" -R /ngencerf-app/ngencerf-docker
-sudo chown "${USER}:pwuser" -R /ngencerf-app/ngencerf-ui
+sudo chown "${USER}:pwuser" -R /ngencerf-app/data-assimilation-engine
+sudo chown "${USER}:pwuser" -R /ngencerf-app/nwm-rte
+sudo chown "${USER}:pwuser" -R /ngencerf-app/nwm-region-mgr
 sudo chown "${USER}:pwuser" -R /ngencerf-app/singularity
 
 git config --global --add safe.directory /ngencerf-app/nwm-automation-scripts
@@ -15,7 +17,9 @@ git config --global --add safe.directory /ngencerf-app/ngen
 git config --global --add safe.directory /ngencerf-app/nwm-cal-mgr
 git config --global --add safe.directory /ngencerf-app/ngen-forcing
 git config --global --add safe.directory /ngencerf-app/nwm-fcst-mgr
-git config --global --add safe.directory /ngencerf-app/nwmn-verf
+git config --global --add safe.directory /ngencerf-app/nwm-verf
 git config --global --add safe.directory /ngencerf-app/ngencerf-server
-git config --global --add safe.directory /ngencerf-app/ngencerf-docker
 git config --global --add safe.directory /ngencerf-app/ngencerf-ui
+git config --global --add safe.directory /ngencerf-app/data-assimilation-engine
+git config --global --add safe.directory /ngencerf-app/nwm-rte
+git config --global --add safe.directory /ngencerf-app/nwm-region-mgr
