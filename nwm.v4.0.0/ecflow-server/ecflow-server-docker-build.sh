9dbcdb863d4e9692bb4141c1c9bbd57ea90f5a77
Max Kipp <jkipp@dewberry.com>
2026-06-08 22:21:04 -0400

    Parameterize Tests of ecFlow Container on Build. Update docs.

--- nwm.v4.0.0/ecflow-server/ecflow-server-docker-build.sh
+++ nwm.v4.0.0/ecflow-server/ecflow-server-docker-build.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-# 
-# Build a docker image of an ecflow server and run its built-in unit tests via `ctest`.
-# 
 
 set -euo pipefail
+
+## 
+## \brief 
+## Build a docker image of an ecflow server and optionally run its built-in unit tests via `ctest`.
+## 
+## \desc
+## Has 1 positional argument and 0 named arguments.
+## 
+## \option RUN_TESTS
+## Dictates whether ecFlow server unit tests are ran after building. Choices: [yes, no].
+## 
+
+
+RUN_TESTS=$1
 
 # BOOST_VERS_DOT=1.89.0
 # BOOST_VERS_UNDER=1_89_0
@@ -19,6 +30,10 @@ docker build \
     -f Dockerfile.ecflow-server .
 echo "Done building ecflow server image."
 
-echo "Running ecflow server's unit tests..."
-docker run -t ecflow-server ctest --test-dir /src/ecflow/build -E '^u_server_authentication$'
-echo "Done with ecflow server tests."
+if [ "${RUN_TESTS,,}" = "yes" ]; then
+    echo "Running ecflow server's unit tests..."
+    docker run -t ecflow-server ctest --test-dir /src/ecflow/build -E '^u_server_authentication$'
+    echo "Done with ecflow server tests."
+else
+    echo "Skipping ecflow server's unit tests."
+fi
