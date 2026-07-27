95d8ecfe03b22620e67fa5a0a436adcbb9643fb1
Max Kipp <jkipp@dewberry.com>
2026-07-27 15:17:28 -0400

    Add sudo to docker calls within the ecflow server build and ecflow server start scripts

--- nwm.v4.0.0/ecflow-server/ecflow-server-start.sh
+++ nwm.v4.0.0/ecflow-server/ecflow-server-start.sh
@@ -21,19 +21,19 @@ fi
 
 # idempotent network creation
 echo "Creating network..."
-docker network inspect "${NETWORK}" >/dev/null 2>&1 || docker network create "${NETWORK}"
+sudo docker network inspect "${NETWORK}" >/dev/null 2>&1 || sudo docker network create "${NETWORK}"
 
 echo "Stopping container if it already exists..."
-docker stop ecflow-server 2>/dev/null || true
+sudo docker stop ecflow-server 2>/dev/null || true
 
 # Start server container
 echo "Starting server container..."
-docker run --rm -d --net "${NETWORK}" --name ecflow-server -p 3141:3141 -v "$(pwd)/data/ecflow:/data/ecflow" ecflow-server /usr/local/ecflow/bin/ecflow_server
+sudo docker run --rm -d --net "${NETWORK}" --name ecflow-server -p 3141:3141 -v "$(pwd)/data/ecflow:/data/ecflow" ecflow-server /usr/local/ecflow/bin/ecflow_server
 echo "Container started."
 
 if [[ "${MODE}" == "test-only" ]]; then
 	echo "Pinging server container from another container..."
-	docker run --rm --net "${NETWORK}" --name ecflow-client ecflow-server ecflow_client --host=ecflow-server --ping
+	sudo docker run --rm --net "${NETWORK}" --name ecflow-client ecflow-server ecflow_client --host=ecflow-server --ping
 	echo "Successful ping to server from another container."
 
 	echo "Ping from local client..."
@@ -41,7 +41,7 @@ if [[ "${MODE}" == "test-only" ]]; then
 	echo "Succeeded ping from local client."
 
 	echo "Stopping server container..."
-	docker stop ecflow-server 2>/dev/null || true
+	sudo docker stop ecflow-server 2>/dev/null || true
 	echo "Stopped server container."
 else
 	echo "Server is running (default mode)."
